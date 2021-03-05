11f220b60acfbdb86d9a98294f81f70035fe61f3
Amir Abushareb <yields@icloud.com>
2021-03-05 17:44:31 +0200

    Implement login command
    The command starts a local server and notifies the user to go to a URL
    that will redirect back to the local server address with the token.

--- commands/login/login.go
+++ commands/login/login.go
@@ -2,7 +2,14 @@ package login
 
 import (
 	"context"
+	"fmt"
+	"net/url"
+	"os"
+	"path/filepath"
 
+	"github.com/airplanedev/cli/pkg/conf"
+	"github.com/airplanedev/cli/pkg/token"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -20,5 +27,50 @@ func New() *cobra.Command {
 
 // Run runs the login command.
 func run(ctx context.Context) error {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		// TODO(amir): Better error message
+		return errors.Wrap(err, "missing homedir")
+	}
+
+	path := filepath.Join(homedir, ".airplane", "config")
+	cfg, err := conf.Read(path)
+
+	if errors.Is(err, conf.ErrMissing) {
+		srv, err := token.NewServer(ctx)
+		if err != nil {
+			return err
+		}
+		defer srv.Close()
+
+		fmt.Println("goto", loginURL(srv.URL()))
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case token := <-srv.Token():
+			cfg.Token = token
+		}
+
+		if err := conf.Save(path, cfg); err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("logged in")
 	return nil
+}
+
+// LoginURL returns the CLI login URL.
+func loginURL(redirect string) string {
+	uri := &url.URL{
+		Scheme: "https",
+		Host:   "app.airplane.local:5000",
+		Path:   "/cli/login",
+		RawQuery: url.Values{
+			"redirect": []string{redirect},
+		}.Encode(),
+	}
+	return uri.String()
 }
