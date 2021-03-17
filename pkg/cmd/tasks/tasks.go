0d4da867101a8f39a9dca21df07cbb26ce85ec3f
Amir Abushareb <yields@icloud.com>
2021-03-17 15:34:49 +0200

    cmd: change layout
    Instead of having misc commands polluting the root command:
    
        airplane list
        airplane create
    
    We will namespace them instead:
    
        airplane tasks list
        airplane tasks create
    
    This will allow us to add "runs show" and "tasks show" command.

---
+++ pkg/cmd/tasks/tasks.go
@@ -0,0 +1,32 @@
+package tasks
+
+import (
+	"github.com/MakeNowJust/heredoc"
+	"github.com/airplanedev/cli/pkg/cli"
+	"github.com/airplanedev/cli/pkg/cmd/tasks/create"
+	"github.com/airplanedev/cli/pkg/cmd/tasks/execute"
+	"github.com/airplanedev/cli/pkg/cmd/tasks/list"
+	"github.com/airplanedev/cli/pkg/cmd/tasks/push"
+	"github.com/spf13/cobra"
+)
+
+// New returns a new cobra command.
+func New(c *cli.Config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tasks",
+		Short: "Manage tasks",
+		Long:  "Manage tasks",
+		Example: heredoc.Doc(`
+			$ airplane tasks create -f mytask.yml
+			$ airplane tasks execute my-task
+			$ airplane tasks push my-task -f mytask.yml
+		`),
+	}
+
+	cmd.AddCommand(create.New(c))
+	cmd.AddCommand(push.New(c))
+	cmd.AddCommand(list.New(c))
+	cmd.AddCommand(execute.New(c))
+
+	return cmd
+}
