8a61b628efa3ec9d9f4cf9545bf6ef33158d9a28
Colin King <me@colinking.co>
2021-03-29 15:23:41 -0500

    hmmm maybe scratch isnt necessary

--- pkg/cmd/tasks/initcmd/scratch.go
+++ pkg/cmd/tasks/initcmd/scratch.go
@@ -4,10 +4,99 @@ import (
 	"os"
 
 	"github.com/AlecAivazis/survey/v2"
+	"github.com/airplanedev/cli/pkg/api"
+	"github.com/airplanedev/cli/pkg/taskdir"
+	"github.com/airplanedev/cli/pkg/utils"
 	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
 )
 
-// TODO
+func initFromScratch(cmd *cobra.Command, cfg config) error {
+	runtime, err := pickRuntime()
+	if err != nil {
+		return err
+	}
+
+	name, err := pickString("Pick a name:")
+	if err != nil {
+		return err
+	}
+
+	description, err := pickString("Pick a description:")
+	if err != nil {
+		return err
+	}
+
+	file := cfg.file
+	if file == "" {
+		file = "airplane.yml"
+	}
+
+	dir, err := taskdir.Open(file)
+	if err != nil {
+		return err
+	}
+	defer dir.Close()
+
+	def := taskdir.Definition{
+		// TODO: choose a unique slug via the Airplane API
+		Slug:        utils.MakeSlug(name),
+		Name:        name,
+		Description: description,
+	}
+
+	if runtime == runtimeKindManual {
+		// TODO: select an image + entrypoint
+	} else {
+		def.Builder, def.BuilderConfig = defaultRuntimeConfig(runtime)
+	}
+
+	if err := dir.WriteDefinition(def); err != nil {
+		return err
+	}
+
+	// TODO: maybe ask for a parameter?
+
+	cmd.Printf(`An Airplane task definition for '%s' has been created in %s!
+
+(TODO: instructions)
+
+Then, deploy it to Airplane with:
+	airplane tasks deploy -f %s`, name, file, file)
+
+	return nil
+}
+
+func defaultRuntimeConfig(runtime runtimeKind) (string, api.BuilderConfig) {
+	switch runtime {
+	case runtimeKindDeno:
+		return "deno", api.BuilderConfig{
+			"entrypoint": "main.ts",
+		}
+	case runtimeKindDockerfile:
+		return "docker", api.BuilderConfig{
+			"dockerfile": "Dockerfile",
+		}
+	case runtimeKindGo:
+		return "docker", api.BuilderConfig{
+			"dockerfile": "Dockerfile",
+		}
+	case runtimeKindManual:
+		return "docker", api.BuilderConfig{
+			"dockerfile": "Dockerfile",
+		}
+	case runtimeKindNode:
+		return "docker", api.BuilderConfig{
+			"dockerfile": "Dockerfile",
+		}
+	case runtimeKindPython:
+		return "docker", api.BuilderConfig{
+			"dockerfile": "Dockerfile",
+		}
+	default:
+		return "", nil
+	}
+}
 
 type runtimeKind string
 
@@ -42,4 +131,19 @@ func pickRuntime() (runtimeKind, error) {
 	}
 
 	return runtimeKind(runtime), nil
+}
+
+func pickString(msg string) (string, error) {
+	var str string
+	if err := survey.AskOne(
+		&survey.Input{
+			Message: msg,
+		},
+		&str,
+		survey.WithStdio(os.Stdin, os.Stderr, os.Stderr),
+	); err != nil {
+		return "", errors.Wrap(err, "")
+	}
+
+	return str, nil
 }
