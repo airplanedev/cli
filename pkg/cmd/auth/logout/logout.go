5476c243ab6c3724334f59a834c71f34f480ba18
Colin King <me@colinking.co>
2021-03-31 13:27:07 -0500

    restructure

--- pkg/cmd/auth/logout/logout.go
+++ pkg/cmd/auth/logout/logout.go
@@ -23,14 +23,16 @@ func New(c *cli.Config) *cobra.Command {
 
 func run(ctx context.Context, c *cli.Config) error {
 	cfg, err := conf.ReadDefault()
-	if err == nil || !errors.Is(err, conf.ErrMissing) {
+	if !errors.Is(err, conf.ErrMissing) {
+		if err != nil {
+			return err
+		}
+
 		delete(cfg.Tokens, c.Client.Host)
 
 		if err := conf.WriteDefault(cfg); err != nil {
 			return err
 		}
-	} else if err != nil {
-		return err
 	}
 
 	logger.Log("Logged out.")
