89340b509745a61228063a4882466b8a3521c517
Colin King <me@colinking.co>
2021-04-07 13:23:03 -0500

    split logic into separate functions

--- pkg/build/remote.go
+++ pkg/build/remote.go
@@ -23,9 +23,32 @@ func Remote(ctx context.Context, dir taskdir.TaskDirectory, client *api.Client) 
 	}
 	defer os.RemoveAll(tmpdir)
 
-	// Archive the root task directory:
-	// TODO: filter out files/directories that match .dockerignore
-	archivePath := path.Join(tmpdir, "airplane-build.tar.gz")
+	archivePath := path.Join(tmpdir, "archive.tar.gz")
+	if err := archiveTaskDir(dir, archivePath); err != nil {
+		return err
+	}
+
+	uploadID, err := uploadArchive(ctx, client, archivePath)
+	if err != nil {
+		return err
+	}
+
+	build, err := client.CreateBuild(ctx, api.CreateBuildRequest{
+		TaskRevisionID: "todo",
+		SourceUploadID: uploadID,
+	})
+	if err != nil {
+		return errors.Wrap(err, "creating build")
+	}
+
+	if err := waitForBuild(ctx, client, build.Build.ID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func archiveTaskDir(dir taskdir.TaskDirectory, archivePath string) error {
 	// mholt/archiver takes a list of "sources" (files/directories) that will
 	// be included in the root of the archive. In our case, we want the root of
 	// the archive to be the contents of the task directory, rather than the
@@ -38,57 +61,50 @@ func Remote(ctx context.Context, dir taskdir.TaskDirectory, client *api.Client) 
 			sources = append(sources, path.Join(dir.DefinitionRootPath(), f.Name()))
 		}
 	}
+
+	// TODO: filter out files/directories that match .dockerignore
 	if err := archiver.Archive(sources, archivePath); err != nil {
 		return errors.Wrap(err, "building archive")
 	}
 
-	// Compute the size of this archive:
-	var sizeBytes int
+	return nil
+}
+
+func uploadArchive(ctx context.Context, client *api.Client, archivePath string) (string, error) {
 	archive, err := os.OpenFile(archivePath, os.O_RDONLY, 0)
 	if err != nil {
-		return errors.Wrap(err, "opening archive file")
+		return "", errors.Wrap(err, "opening archive file")
 	}
 	defer archive.Close()
-	if info, err := archive.Stat(); err != nil {
-		return errors.Wrap(err, "stat on archive file")
-	} else {
-		sizeBytes = int(info.Size())
-	}
 
-	// Upload the archive to Airplane:
+	info, err := archive.Stat()
+	if err != nil {
+		return "", errors.Wrap(err, "stat on archive file")
+	}
+	sizeBytes := int(info.Size())
+
 	upload, err := client.CreateBuildUpload(ctx, api.CreateBuildUploadRequest{
 		SizeBytes: sizeBytes,
 	})
 	if err != nil {
-		return errors.Wrap(err, "creating upload")
+		return "", errors.Wrap(err, "creating upload")
 	}
 
 	logger.Debug("Uploaded archive to id=%s at url=%s", upload.Upload.ID, upload.Upload.URL)
 
 	req, err := http.NewRequestWithContext(ctx, "PUT", upload.WriteOnlyURL, archive)
 	if err != nil {
-		return errors.Wrap(err, "creating GCS upload request")
+		return "", errors.Wrap(err, "creating GCS upload request")
 	}
 	req.Header.Add("X-Goog-Content-Length-Range", fmt.Sprintf("0,%d", sizeBytes))
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return errors.Wrap(err, "uploading to GCS")
+		return "", errors.Wrap(err, "uploading to GCS")
 	}
 	defer resp.Body.Close()
 
-	build, err := client.CreateBuild(ctx, api.CreateBuildRequest{
-		TaskRevisionID: "todo",
-		SourceUploadID: upload.Upload.ID,
-	})
-	if err != nil {
-		return errors.Wrap(err, "creating build")
-	}
-
-	if err := waitForBuild(ctx, client, build.Build.ID); err != nil {
-		return err
-	}
-
-	return nil
+	return upload.Upload.ID, nil
 }
 
 func waitForBuild(ctx context.Context, client *api.Client, buildID string) error {
