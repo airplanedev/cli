6c29c3549f3e37a5d50e73252cb41f3944b09d7f
Joshua Ma <me@joshma.com>
2021-04-14 16:09:07 -0700

    Revert "Fix logger colors to support format strings"
    This reverts commit bc02cd60b465fc8dbcf8c06b38ae5ad98d3d8f8b.

--- pkg/logger/colors.go
+++ pkg/logger/colors.go
@@ -1,23 +1,12 @@
 package logger
 
-import (
-	"fmt"
-
-	"github.com/fatih/color"
-)
+import "github.com/fatih/color"
 
 var (
-	Gray   = ColorSprintfFunc(color.FgHiBlack)
-	Blue   = ColorSprintfFunc(color.FgHiBlue)
-	Red    = ColorSprintfFunc(color.FgHiRed)
-	Yellow = ColorSprintfFunc(color.FgHiYellow)
-	Green  = ColorSprintfFunc(color.FgHiGreen)
-	Bold   = ColorSprintfFunc(color.Bold)
+	Gray   = color.New(color.FgHiBlack).SprintFunc()
+	Blue   = color.New(color.FgHiBlue).SprintFunc()
+	Red    = color.New(color.FgHiRed).SprintFunc()
+	Yellow = color.New(color.FgHiYellow).SprintFunc()
+	Green  = color.New(color.FgHiGreen).SprintFunc()
+	Bold   = color.New(color.Bold).SprintFunc()
 )
-
-func ColorSprintfFunc(c color.Attribute) func(msg string, args ...interface{}) string {
-	colorSprint := color.New(c).SprintFunc()
-	return func(msg string, args ...interface{}) string {
-		return colorSprint(fmt.Sprintf(msg, args...))
-	}
-}
