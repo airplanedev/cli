93125ff96714e77a92697268a99a9f86ba9e12b2
Amir Abushareb <yields@icloud.com>
2021-05-25 21:00:15 +0300

    Update error message
    Co-authored-by: Colin <colinking@users.noreply.github.com>

--- pkg/runtime/javascript/javascript.go
+++ pkg/runtime/javascript/javascript.go
@@ -39,7 +39,7 @@ func (r Runtime) Generate(t api.Task) ([]byte, error) {
 	var buf bytes.Buffer
 
 	if err := code.Execute(&buf, args); err != nil {
-		return nil, fmt.Errorf("typescript: template execute - %w", err)
+		return nil, fmt.Errorf("javascript: template execute - %w", err)
 	}
 
 	return buf.Bytes(), nil
