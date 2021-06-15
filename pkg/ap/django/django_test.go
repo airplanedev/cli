29073a304913eaf8df8d6ae1209457fb5987ad9c
Amir Abushareb <yields@icloud.com>
2021-06-15 19:42:20 +0300

    Add stderr test

--- pkg/ap/django/django_test.go
+++ pkg/ap/django/django_test.go
@@ -9,11 +9,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const managepy = `#!/usr/bin/env python3
-
+const (
+	managepy = `#!/usr/bin/env python3
 print("task_2")
-print("task_1")
-`
+print("task_1")`
+	boom = `#!/usr/bin/env python3
+	\\
+	`
+)
 
 func TestFramework(t *testing.T) {
 	t.Run("missing manage.py", func(t *testing.T) {
@@ -61,6 +64,26 @@ func TestFramework(t *testing.T) {
 		cmds, err := f.ListCommands()
 		assert.NoError(err)
 		assert.Equal([]string{"task_1", "task_2"}, cmds)
+	})
+
+	t.Run("list command error", func(t *testing.T) {
+		var assert = require.New(t)
+		var root = tmpdir(t)
+		var bin = filepath.Join(root, "manage.py")
+
+		err := os.WriteFile(
+			bin,
+			[]byte(boom),
+			0770,
+		)
+		assert.NoError(err)
+
+		f, err := Open(root)
+		assert.NoError(err)
+
+		_, err = f.ListCommands()
+		assert.Error(err)
+		assert.Contains(err.Error(), "unexpected character")
 	})
 }
 
