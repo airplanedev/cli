6ef97baa37a2181a96b4d6fc459a3a5b641b9e99
Colin King <me@colinking.co>
2021-06-20 09:28:32 -0400

    tweak test

--- pkg/conf/conf_test.go
+++ pkg/conf/conf_test.go
@@ -37,33 +37,33 @@ func TestConfig(t *testing.T) {
 		assert.Equal("foo", cfg.Tokens["airplane.dev"])
 	})
 
-	// t.Run("overwrite", func(t *testing.T) {
-	// 	var assert = require.New(t)
-	// 	var homedir = tempdir(t)
-	// 	var path = filepath.Join(homedir, ".airplane", "config")
+	t.Run("overwrite", func(t *testing.T) {
+		var assert = require.New(t)
+		var homedir = tempdir(t)
+		var path = filepath.Join(homedir, ".airplane", "config")
 
-	// 	{
-	// 		err := Write(path, Config{
-	// 			Tokens: map[string]string{"airplane.dev": "foo"},
-	// 		})
-	// 		assert.NoError(err)
+		{
+			err := Write(path, Config{
+				Tokens: map[string]string{"airplane.dev": "foo"},
+			})
+			assert.NoError(err)
 
-	// 		cfg, err := Read(path)
-	// 		assert.NoError(err)
-	// 		assert.Equal("foo", cfg.Tokens["airplane.dev"])
-	// 	}
+			cfg, err := Read(path)
+			assert.NoError(err)
+			assert.Equal("foo", cfg.Tokens["airplane.dev"])
+		}
 
-	// 	{
-	// 		err := Write(path, Config{
-	// 			Tokens: map[string]string{"airplane.dev": "baz"},
-	// 		})
-	// 		assert.NoError(err)
+		{
+			err := Write(path, Config{
+				Tokens: map[string]string{"airplane.dev": "baz"},
+			})
+			assert.NoError(err)
 
-	// 		cfg, err := Read(path)
-	// 		assert.NoError(err)
-	// 		assert.Equal("baz", cfg.Tokens["airplane.dev"])
-	// 	}
-	// })
+			cfg, err := Read(path)
+			assert.NoError(err)
+			assert.Equal("baz", cfg.Tokens["airplane.dev"])
+		}
+	})
 }
 
 func tempdir(t testing.TB) string {
