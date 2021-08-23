ddcab25be1f4f79b60628815a3754f386f8bfada
Colin King <me@colinking.co>
2021-08-23 19:37:22 -0400

    Print CLI version when run with --debug

--- pkg/cmd/root/root.go
+++ pkg/cmd/root/root.go
@@ -65,6 +65,10 @@ func New() *cobra.Command {
 			logger.EnableDebug = cfg.DebugMode
 			trap.Printf = logger.Log
 
+			// Log the version everytime the CLI is run with --debug. This means we can align
+			// debugging output from a customer with a specific release of the CLI.
+			logger.Debug(version.Version())
+
 			return nil
 		},
 		PersistentPostRun: func(cmd *cobra.Command, args []string) {
